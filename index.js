f08beb5826dc42416169eca6637a43060e2d4882
zycroq <35395363+zycroq@users.noreply.github.com>
2018-01-13 12:04:35 +0100

    Update index.js

--- index.js
+++ index.js
@@ -43,8 +43,8 @@ bot.on("message", function(message) {
                 message.channel.send(message.author + ", there you have a new account.");
                 }); 
              break;
-             case "ping":
-             message.channel.send("Pong!");
+             case "news":
+             message.channel.send("| 13.01.2018 | We have now a StrayBoots bot. Made by @wrymex ❄🔗#2371 & @maiu🎉#1152 ");
             break;
         default:
             message.channel.send("Sorry, but i cant find your command.");
