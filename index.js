44018e37332e8e22df6be825125f34d8d223e71b
zycroq <35395363+zycroq@users.noreply.github.com>
2018-01-14 10:07:48 +0100

    Update index.js

--- index.js
+++ index.js
@@ -51,7 +51,7 @@ bot.on("message", function(message) {
             }
             message.author.send(splitted);
             message.author.send("If you believe your account is not working, send a message to Wrymex.");
-            message.channel.send(message.author + ", you have now received an NFA user! \ nWait ** 10 minutes ** before trying this command again.");
+            message.channel.send(message.author + ", you have now received an NFA user! \ Wait ** 10 minutes ** before trying this command again.");
             }); 
          break;
              case "changelog":
@@ -60,7 +60,32 @@ bot.on("message", function(message) {
             break;
              case "staff":
              message.channel.send("Founder | TEST");
+             message.channel.send("");
              break;
+             case "flixalt":
+             if (talkedRecently.has(message.author.id))
+         return;
+       
+       // Adds the user to the set so that they can't talk for 2.5 seconds
+       talkedRecently.add(message.author.id);
+       setTimeout(() => {
+         // Removes the user from the set after 2.5 seconds
+         talkedRecently.delete(message.author.id);
+       }, 600000);
+             var request = require('request');
+             var a = [];
+             request('https://pastebin.com/raw/iPH6yLUk',function (error,response,body) 
+                 {
+                  if (!error && response.statusCode == 200) {
+                  a.push(body);
+                  var arr = a.toString().split("\n");
+                  var splitted = arr[Math.floor(Math.random() * arr.length)];
+                 }
+                 message.author.send(splitted);
+                 message.author.send("If you believe your account is not working, send a message to Wrymex.");
+                 message.channel.send(message.author + ", you have now received an Netflix account! \ Wait ** 10 minutes ** before trying this command again.");
+                 }); 
+              break;
         default:
             message.channel.send("Unknown command, please use the ?help command.");
             break;
