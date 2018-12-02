6c85612df105e1f659127770c0685badcd865c56
zycroq <35395363+zycroq@users.noreply.github.com>
2018-12-02 14:54:13 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,28 +1,19 @@
 const Discord = require("discord.js");
 
-const PREFIX = "?";
-
-const TOKEN = "NDAwMzYzODk3NDM1MTkzMzQ0.DTtTIA.ws-p5bMi6cZLrknParstNtbyXPM";
-
-var bot = new Discord.Client();
-
-const list = bot.guilds.map(g => `Name: ${g.name} | ID: ${g.id}`).join(`\n`);
+const PREFIX = "!";
+const OwnerID = "461100606334173184";
 
 const talkedRecently = new Set();
 
-bot.on("message", function(message) {
-    console.log(message.content);
-    if (message.author.id == "249838434682667009" || message.author.id == "391300271239593984") 
-    if (message.content === "leave") {
-        bot.guilds.get("393280620249481228").leave();
-    }
+var bot = new Discord.Client();
 
-    if (message.content === "servers") {
-        message.channel.send(list);
-    }
+bot.on("ready", function() {
+    console.log("EmpathyGEN | Ready");
+
+    bot.user.setStatus('dnd')
+
+    bot.user.setGame('generating accounts')
 });
-
-bot.login(TOKEN);
 
 bot.on("message", function(message) {
     if (message.author.equals(bot.user)) return;
@@ -32,15 +23,17 @@ bot.on("message", function(message) {
     var args = message.content.substring(PREFIX.length).split(" ");
 
     switch (args[0].toLowerCase()) {
-        case "info":
-            const embed = new Discord.RichEmbed()
-            .setTitle("StrayBoots Official Discord Bot")
-            .setDescription("If you want a new alt account/second account write >getalt")
-            .setThumbnail("http://householdairfresheners.com/wp-content/uploads/2017/02/Perfect-S-Logo-Design-75-For-Your-free-logo-designer-with-S-Logo-Design.jpg")
-            .setFooter("Join our discord here: https://discord.gg/hQCbvAk");
-            message.channel.send({embed});
-            break;
-        case "getalt":
+        case "help":
+        var embed = new Discord.RichEmbed()
+        .setTitle("EmpathyGEN | Command List")
+        .addField("!fortnite", "Send's you a **Fortnite Account**.")
+        .addField("!spotify", "Send's you a **Spotify Account**.")
+        .setColor("#005ce6")
+        .setFooter("EmpathyV1 - Command List", message.author.avatarURL)
+        .setThumbnail(message.author.avatarURL)
+        message.channel.sendEmbed(embed);
+        break;
+        case "spotify":
         if (talkedRecently.has(message.author.id))
     return;
   
@@ -52,80 +45,32 @@ bot.on("message", function(message) {
   }, 600000);
         var request = require('request');
         var a = [];
-        request('https://pastebin.com/raw/F2qt099n',function (error,response,body) 
+        request('https://pastebin.com/raw/4CYcwHjk',function (error,response,body) 
             {
              if (!error && response.statusCode == 200) {
              a.push(body);
              var arr = a.toString().split("\n");
              var splitted = arr[Math.floor(Math.random() * arr.length)];
             }
-            message.author.send(splitted);
-            message.author.send("If you believe your account is not working, send a message to Wrymex.");
-            message.channel.send(message.author + ", you have now received an NFA user! \ Wait ** 10 minutes ** before trying this command again.");
-            }); 
+            var embed = new Discord.RichEmbed()
+            .setTitle("Empathy » Spotify")
+            .setDescription(splitted)
+            .setColor("#ff0000")
+            message.author.sendEmbed(embed);
+            var embed = new Discord.RichEmbed()
+            .setTitle("Empathy » Spotify")
+            .setDescription(message.author + ", a Spotify account has been sent to you.")
+            .setColor("#009933")
+            message.channel.sendEmbed(embed);
+        }); 
          break;
-             case "changelog":
-            message.channel.send("| 13.01.2018 | StrayBoots bot founded");
-            message.channel.send("| 14.01.2018 | The getalt command got 10 minutes cooldown.");
-            break;
-             case "staff":
-             message.channel.send("Founder | TEST");
-             message.channel.send("");
-             break;
-             case "flixalt":
-             if (talkedRecently.has(message.author.id))
-         return;
-       
-       // Adds the user to the set so that they can't talk for 2.5 seconds
-       talkedRecently.add(message.author.id);
-       setTimeout(() => {
-         // Removes the user from the set after 2.5 seconds
-         talkedRecently.delete(message.author.id);
-       }, 600000);
-             var request = require('request');
-             var a = [];
-             request('https://pastebin.com/raw/iPH6yLUk',function (error,response,body) 
-                 {
-                  if (!error && response.statusCode == 200) {
-                  a.push(body);
-                  var arr = a.toString().split("\n");
-                  var splitted = arr[Math.floor(Math.random() * arr.length)];
-                 }
-                 message.author.send(splitted);
-                 message.author.send("If you believe your account is not working, send a message to Wrymex.");
-                 message.channel.send(message.author + ", you have now received an Netflix account! \ Wait ** 10 minutes ** before trying this command again.");
-                 }); 
-              break;
-              case "removecooldown":
-              if (message.author.id == "249838434682667009" || message.author.id == "391300271239593984") {
-
-              if(args.length == 0) {
-                  message.channel.send(message.author + ", please provide a user id!");
-              } else {
-                  talkedRecently.delete(args[1]);
-                  message.channel.send(message.author + ", that user was now removed from the cooldown!");
-              }
-
-              } else {
-                  message.channel.send(message.author + ", you do not have permissions to perform this command!");
-              }
-               break;
-               case "removecd":
-               if (message.author.id == "249838434682667009" || message.author.id == "391300271239593984") {
- 
-               if(args.length == 0) {
-                   message.channel.send(message.author + ", please provide a user id!");
-               } else {
-                   talkedRecently.delete(args[1]);
-                   message.channel.send(message.author + ", that user was now removed from the cooldown!");
-               }
- 
-               } else {
-                   message.channel.send(message.author + ", you do not have permissions to perform this command!");
-               }
-                break;
         default:
-            message.channel.send("Unknown command, please use the ?help command.");
-            break;
+        var embed = new Discord.RichEmbed()
+            .setTitle("Empathy » Error")
+            .setDescription("Invalid command.")
+            .setColor("#9900ff")
+            message.channel.sendEmbed(embed);
     }
 });
+
+bot.login(TOKEN);
